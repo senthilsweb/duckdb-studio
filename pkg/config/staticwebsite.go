8c32c881c5ba84ed65bd7a7e2d24da4103393df3
senthilsweb <senthilsweb@gmail.com>
2024-03-20 21:45:40 -0400

    First full commit

--- pkg/config/staticwebsite.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type StaticWebSite struct {
-	Enable    bool   `default:"false"`
-	WebFolder string `default:"/opt/{{app}}/www"`
-	IndexPage string `default:"index.html"`
-	Route     string `default:"/"`
-}
